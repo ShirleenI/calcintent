9c1ec3d188326700655a01218891b9ac29ba5e70
ShirleenI <irulumashirleen@gmail.com>
2022-08-03 16:13:14 +0300

    initial commit

--- app/src/main/java/com/example/myfirstapp/IntentActivity.kt
+++ app/src/main/java/com/example/myfirstapp/IntentActivity.kt
@@ -1,11 +1,108 @@
 package com.example.myfirstapp
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class IntentActivity : AppCompatActivity() {
+    lateinit var Buttonsms:Button
+    lateinit var Buttoncall:Button
+    lateinit var Buttoncamera:Button
+    lateinit var Buttonshare:Button
+    lateinit var Buttonemail:Button
+    lateinit var Buttonstk:Button
+    lateinit var Buttondial:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+        Buttonsms=findViewById(R.id.Btn_sms)
+        Buttoncall=findViewById(R.id.Btn_call)
+        Buttoncamera=findViewById(R.id.Btn_cam)
+        Buttonshare=findViewById(R.id.Btn_share)
+        Buttonemail=findViewById(R.id.Btn_email)
+        Buttonstk=findViewById(R.id.Btn_stk)
+        Buttondial=findViewById(R.id.Btn_dial)
+
+        Buttonsms.setOnClickListener {
+            val uri = Uri.parse("smsto:0745678954")
+
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            intent.putExtra("Hello", "How is today's weather")
+
+            startActivity(intent)
+        }
+        Buttoncamera.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            startActivityForResult(takePictureIntent, 1)
+
+        }
+        Buttoncall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+918511812660"))
+
+            if (ContextCompat.checkSelfPermission(
+                    this@IntentActivity,
+                    arrayOf(android.Manifest.permission.CALL_PHONE).toString()
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@IntentActivity,
+                    arrayOf(android.Manifest.permission.CALL_PHONE.toString()),
+                    
+                    1
+                )
+            } else {
+                startActivity(intent)
+            }
+
+
+        }
+        Buttonshare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+
+            shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            shareIntent.type = "text/plain"
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app!")
+
+            startActivity(shareIntent)
+
+        }
+        Buttonstk.setOnClickListener {
+            val simToolKitLaunchIntent =
+                applicationContext.packageManager.getLaunchIntentForPackage("com.android.stk")
+
+            simToolKitLaunchIntent?.let { startActivity(it) }
+
+        }
+        Buttondial.setOnClickListener {
+            val phone = "+34666777888"
+
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+
+            startActivity(intent)
+
+        }
+        Buttonemail.setOnClickListener {
+
+            val emailIntent =
+                Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "abc@gmail.com", null))
+
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Body")
+
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+        }
+
     }
 }
--- app/src/main/java/com/example/myfirstapp/WebActivity.kt
+++ app/src/main/java/com/example/myfirstapp/WebActivity.kt
@@ -1,11 +1,36 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class WebActivity : AppCompatActivity() {
+    lateinit var wbview:WebView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        wbview=findViewById(R.id.webView)
+        webview()
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webview(){
+        wbview.webViewClient= WebViewClient()
+        wbview.apply {
+            loadUrl("https://www.instagram.com/")
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+        }
+        fun onBackPressed() { if (wbview.canGoBack())wbview.goBack()
+            super.onBackPressed()
+        }
+
+
+
     }
 }
